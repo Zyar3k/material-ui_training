ff2fc8fd9f0559aec8585856248c66e1eb1c68c0
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-28 16:52:57 +0200

    Mui buttons

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,71 @@
+import { Button, Stack, IconButton } from "@mui/material";
+import SendIcon from "@mui/icons-material/Send";
+
+const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://goggle.com">
+          text
+        </Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          primary
+        </Button>
+        <Button variant="contained" color="secondary">
+          secondary
+        </Button>
+        <Button variant="contained" color="error">
+          error
+        </Button>
+        <Button variant="contained" color="warning">
+          warning
+        </Button>
+        <Button variant="contained" color="info">
+          info
+        </Button>
+        <Button variant="contained" color="success">
+          success
+        </Button>
+      </Stack>
+      <Stack display="block" spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          color="primary"
+          disableRipple
+          startIcon={<SendIcon />}
+          onClick={() => alert("clicked")}
+        >
+          Send
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          endIcon={<SendIcon />}
+        >
+          Send
+        </Button>
+        <IconButton aria-label="send" color="info" size="large">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default MuiButton;
