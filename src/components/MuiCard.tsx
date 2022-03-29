b17bbb84c914f9eb16ceb841e26bac82311af64c
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-29 17:01:24 +0200

    Mui card

---
+++ src/components/MuiCard.tsx
@@ -0,0 +1,44 @@
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  CardMedia,
+} from "@mui/material";
+
+const MuiCard = () => {
+  return (
+    <Box width="360px">
+      <Card>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            React
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, in
+            vel iure autem hic neque voluptatum? Totam, omnis veritatis.
+            Praesentium.
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
+  );
+};
+
+export default MuiCard;
