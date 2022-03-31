93c704fdd5286a1c41d8eabddf8f94e37415cf67
Arkadiusz Zygan <a.zygan@gmail.com>
2022-03-31 12:58:41 +0200

    Mui breadcrumbs

---
+++ src/components/MuiBreadcrumbs.tsx
@@ -0,0 +1,29 @@
+import { Box, Breadcrumbs, Link, Typography } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+const MuiBreadcrumbs = () => {
+  return (
+    <Box m={2}>
+      <Breadcrumbs
+        aria-label="breadcrumb"
+        separator={<NavigateNextIcon fontSize="small" />}
+        maxItems={2}
+        // itemsAfterCollapse={2}
+        itemsBeforeCollapse={2}
+      >
+        <Link underline="hover" href="#">
+          Home
+        </Link>
+        <Link underline="hover" href="#">
+          Library
+        </Link>
+        <Link underline="hover" href="#">
+          Data
+        </Link>
+        <Typography color="text.primary">Shoes</Typography>
+      </Breadcrumbs>
+    </Box>
+  );
+};
+
+export default MuiBreadcrumbs;
