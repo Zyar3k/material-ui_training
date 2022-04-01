9621f0c76a24cf80935078e430509f0895519759
Arkadiusz Zygan <a.zygan@gmail.com>
2022-04-01 17:18:39 +0200

    Mui basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,98 @@
 import "./App.css";
-// import MuiDialog from "./components/MuiDialog";
-// import MuiProgress from "./components/MuiProgress";
+import MuiDialog from "./components/MuiDialog";
+import MuiProgress from "./components/MuiProgress";
 import MuiSkeleton from "./components/MuiSkeleton";
-// import MuiAlert from "./components/MuiAlert";
-// import MuiSnackbar from "./components/MuiSnackbar";
-// import MuiTable from "./components/MuiTable";
-// import MuiAvatar from "./components/MuiAvatar";
-// import MuiBadge from "./components/MuiBadge";
-// import MuiChip from "./components/MuiChip";
-// import MuiTooltip from "./components/MuiTooltip";
-// import MuiList from "./components/MuiList";
-// import MuiBottomNavigation from "./components/MuiBottomNavigation";
-// import MuiDrawer from "./components/MuiDrawer";
-// import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
-// import MuiLink from "./components/MuiLink";
-// import MuiNavbar from "./components/MuiNavbar";
-// import MuiSpeedDial from "./components/MuiSpeedDial";
-// import MuiImageList from "./components/MuiImageList";
-// import MuiAccordion from "./components/MuiAccordion";
-// import MuiCard from "./components/MuiCard";
-// import MuiLayout from "./components/MuiLayout";
-// import MuiAutocomplete from "./components/MuiAutocomplete";
-// import MuiRating from "./components/MuiRating";
-// import MuiSwitch from "./components/MuiSwitch";
-// import MuiCheckbox from "./components/MuiCheckbox";
-// import MuiRadioButton from "./components/MuiRadioButton";
-// import MuiSelect from "./components/MuiSelect";
-// import MuiTextField from "./components/MuiTextField";
-// import MuiButton from "./components/MuiButton";
-// import MuiTypography from "./components/MuiTypography";
+import MuiAlert from "./components/MuiAlert";
+import MuiSnackbar from "./components/MuiSnackbar";
+import MuiTable from "./components/MuiTable";
+import MuiAvatar from "./components/MuiAvatar";
+import MuiBadge from "./components/MuiBadge";
+import MuiChip from "./components/MuiChip";
+import MuiTooltip from "./components/MuiTooltip";
+import MuiList from "./components/MuiList";
+import MuiBottomNavigation from "./components/MuiBottomNavigation";
+import MuiDrawer from "./components/MuiDrawer";
+import MuiBreadcrumbs from "./components/MuiBreadcrumbs";
+import MuiLink from "./components/MuiLink";
+import MuiNavbar from "./components/MuiNavbar";
+import MuiSpeedDial from "./components/MuiSpeedDial";
+import MuiImageList from "./components/MuiImageList";
+import MuiAccordion from "./components/MuiAccordion";
+import MuiCard from "./components/MuiCard";
+import MuiLayout from "./components/MuiLayout";
+import MuiAutocomplete from "./components/MuiAutocomplete";
+import MuiRating from "./components/MuiRating";
+import MuiSwitch from "./components/MuiSwitch";
+import MuiCheckbox from "./components/MuiCheckbox";
+import MuiRadioButton from "./components/MuiRadioButton";
+import MuiSelect from "./components/MuiSelect";
+import MuiTextField from "./components/MuiTextField";
+import MuiButton from "./components/MuiButton";
+import MuiTypography from "./components/MuiTypography";
+import { Divider } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
-      {/* <MuiTypography /> */}
-      {/* <MuiButton /> */}
-      {/* <MuiTextField /> */}
-      {/* <MuiSelect /> */}
-      {/* <MuiRadioButton /> */}
-      {/* <MuiCheckbox /> */}
-      {/* <MuiSwitch /> */}
-      {/* <MuiRating /> */}
-      {/* <MuiAutocomplete /> */}
-      {/* <MuiLayout /> */}
-      {/* <MuiCard /> */}
-      {/* <MuiAccordion /> */}
-      {/* <MuiImageList /> */}
-      {/* <MuiNavbar /> */}
-      {/* <MuiDrawer />
-      <MuiSpeedDial /> */}
-      {/* <MuiLink /> */}
-      {/* <MuiBreadcrumbs /> */}
-      {/* <MuiAvatar /> */}
-      {/* <MuiBadge /> */}
-      {/* <MuiBottomNavigation /> */}
-      {/* <MuiList /> */}
-      {/* <MuiChip /> */}
-      {/* <MuiTooltip /> */}
-      {/* <MuiTable /> */}
-      {/* <MuiAlert /> */}
-      {/* <MuiSnackbar /> */}
-      {/* <MuiDialog /> */}
-      {/* <MuiProgress /> */}
+      <MuiNavbar />
+      <MuiTypography />
+      <Divider sx={{ m: "100px" }} />
+      <MuiButton />
+      <Divider sx={{ m: "100px" }} />
+      <MuiTextField />
+      <Divider sx={{ m: "100px" }} />
+      <MuiSelect />
+      <Divider sx={{ m: "100px" }} />
+      <MuiRadioButton />
+      <Divider sx={{ m: "100px" }} />
+      <MuiCheckbox />
+      <Divider sx={{ m: "100px" }} />
+      <MuiSwitch />
+      <Divider sx={{ m: "100px" }} />
+      <MuiRating />
+      <Divider sx={{ m: "100px" }} />
+      <MuiAutocomplete />
+      <Divider sx={{ m: "100px" }} />
+      <MuiLayout />
+      <Divider sx={{ m: "100px" }} />
+      <MuiCard />
+      <Divider sx={{ m: "100px" }} />
+      <MuiAccordion />
+      <Divider sx={{ m: "100px" }} />
+      <MuiImageList />
+      <Divider sx={{ m: "100px" }} />
+      <MuiDrawer />
+      <Divider sx={{ m: "100px" }} />
+      <MuiSpeedDial />
+      <Divider sx={{ m: "100px" }} />
+      <MuiLink />
+      <Divider sx={{ m: "100px" }} />
+      <MuiBreadcrumbs />
+      <Divider sx={{ m: "100px" }} />
+      <MuiAvatar />
+      <Divider sx={{ m: "100px" }} />
+      <MuiBadge />
+      <Divider sx={{ m: "100px" }} />
+      <MuiList />
+      <Divider sx={{ m: "100px" }} />
+      <MuiChip />
+      <Divider sx={{ m: "100px" }} />
+      <MuiTooltip />
+      <Divider sx={{ m: "100px" }} />
+      <MuiTable />
+      <Divider sx={{ m: "100px" }} />
+      <MuiAlert />
+      <Divider sx={{ m: "100px" }} />
+      <MuiSnackbar />
+      <Divider sx={{ m: "100px" }} />
+      <MuiDialog />
+      <Divider sx={{ m: "100px" }} />
+      <MuiProgress />
+      <Divider sx={{ m: "100px" }} />
       <MuiSkeleton />
+      <Divider sx={{ m: "100px" }} />
+      <MuiBottomNavigation />
+      <Divider sx={{ m: "100px" }} />
     </div>
   );
 }
